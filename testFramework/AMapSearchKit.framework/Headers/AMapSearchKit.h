03e0fe3141316bdc5dabb0f3dc1af06934a4e3d2
guangbin <guangbin79@gmail.com (mailto:guangbin79@gmail.com)>
2015-05-13 14:20:25 +0800

    first commit

---
+++ testFramework/AMapSearchKit.framework/Headers/AMapSearchKit.h
@@ -0,0 +1,20 @@
+//
+//  AMapSearchKit.h
+//  AMapSearchKit
+//
+//  Created by 成雨航 on 15/5/13.
+//  Copyright (c) 2015年 成雨航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for AMapSearchKit.
+FOUNDATION_EXPORT double AMapSearchKitVersionNumber;
+
+//! Project version string for AMapSearchKit.
+FOUNDATION_EXPORT const unsigned char AMapSearchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMapSearchKit/PublicHeader.h>
+
+#import <AMapSearchKit/testObj.h>
---
+++ testFramework/AMapSearchKit.framework/Headers/testObj.h
@@ -0,0 +1,13 @@
+//
+//  testObj.h
+//  AMapSearchKit
+//
+//  Created by 成雨航 on 15/5/13.
+//  Copyright (c) 2015年 成雨航. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testObj : NSObject
+
+@end
