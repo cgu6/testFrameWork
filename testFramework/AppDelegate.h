4c0c75c202ab9a239bbd847653cfc33b89357e74
guangbin <guangbin79@gmail.com (mailto:guangbin79@gmail.com)>
2015-05-13 11:29:20 +0800

    Initial Commit

---
+++ testFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFramework
+//
+//  Created by 成雨航 on 15/5/13.
+//  Copyright (c) 2015年 成雨航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testFramework
+//
+//  Created by 成雨航 on 15/5/13.
+//  Copyright (c) 2015年 成雨航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
